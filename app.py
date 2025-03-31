fd85b6caeb82e47896c3699a07f1bc61af39b94a
lordDURGAPRASAD <138786631+lordDURGAPRASAD@users.noreply.github.com>
2025-03-31 16:31:09 +0530

    king durgas blessings

--- app.py
+++ app.py
@@ -170,4 +170,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     # For development testing only:
-    # socketio.run(app, host="0.0.0.0", port=8000, debug=True)
+    socketio.run(app, host="127.0.0.1", port=8000, debug=True)
